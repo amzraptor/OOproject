25724d5a7eff65ee03ada6978bc4c346c0836e64
Mark Vaughn <mwvaughn1@cougars.ccis.edu>
2012-10-29 20:09:53 -0500

    fixed alignment of header and changed the slideshow

--- jb/homepage.php
+++ jb/homepage.php
@@ -9,33 +9,15 @@ setInterval(function() {
     .fadeIn(1000)
     .end()
     .appendTo('#slideshow');
-},  6000);
-
-$("#slideshow2 div:gt(0)").hide();
-
-setInterval(function() { 
-  $('#slideshow2 div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow2');
-},  6000);
-
+},  3000);
 </script>
-<div id="body_background" style="height:100%;width:100%;">
 
-		<div id="slideshow" style="float:left;">
-			<div><img name ="homepage_products[]" value="product_1" src="images/JewelryPic_01.png" width="400px" height="400px"></div>
-			<div><img name ="homepage_products[]" value="product_2" src="images/JewelryPic_02.png"  width="400px" height="400px"></div>
-			<div><img name ="homepage_products[]" value="product_3" src="images/JewelryPic_03.png"  width="400px" height="400px"></div>
-			<div><img name ="homepage_products[]" value="product_4" src="images/JewelryPic_04.png" width="400px" height="400px"></div>
-		</div>	
-	
-		<div id="slideshow2" style="float:right;">
-			<div><img name ="homepage_stores[]" value="store_1" src="images/Store_01.png"  width="400px" height="400px"></div>
-			<div><img name ="homepage_stores[]" value="store_2" src="images/Store_02.png"  width="400px" height="400px"></div>
-			<div><img name ="homepage_stores[]" value="store_3" src="images/Store_03.png" width="400px" height="400px"></div>
-		</div>	
+<div id="body_background">
 
+		<div style="margin-left:40px;" id="slideshow">
+			<div><img name ="homepage_products[]" value="product_1" src="images/JewelryPic_01.png"></div>
+			<div><img name ="homepage_products[]" value="product_2" src="images/JewelryPic_02.png"></div>
+			<div><img name ="homepage_products[]" value="product_3" src="images/JewelryPic_03.png"></div>
+			<div><img name ="homepage_products[]" value="product_4" src="images/JewelryPic_04.png"></div>
+		</div>	
 </div>
