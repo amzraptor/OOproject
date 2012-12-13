ec5ea22e92edf92019433c80fa015a0a2900bb80
Zaki <amz_raptor@hotmail.com>
2012-12-12 23:47:13 -0600

    useless

--- jb/pages/purchase_backend.php
+++ jb/pages/purchase_backend.php
@@ -36,6 +36,8 @@ case "order":
 			//$arr = array($product_id);
 			save_product_to_invoice($store_id, $invoice_id, $product_id, $product_name, $price, $qty);
 		}
+
+		
 		echo json_encode($arr);
 		break;
 
