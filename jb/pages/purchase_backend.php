4761af03cb82d201d858f9da3252e2ae5f0e595f
Zaki <amz_raptor@hotmail.com>
2012-12-13 02:39:52 -0600

    added

--- jb/pages/purchase_backend.php
+++ jb/pages/purchase_backend.php
@@ -26,6 +26,7 @@ case "order":
 		//saving the cart itself
 
 		$invoice_id = intval(get_last_invoice_id()); //get last MAX invoice_id
+
 		foreach($_SESSION['cart'] as $product_id => $qty)
 		{
 			$store_id = intval(get_product_store_id($product_id));
@@ -34,12 +35,26 @@ case "order":
 			$product_id = intval($product_id);
 			$qty = intval($qty);
 			//$arr = array($product_id);
+			//$message .= "Name: ".$product_name."         Quantity: ".$qty."               Price: ".$price;
 			save_product_to_invoice($store_id, $invoice_id, $product_id, $product_name, $price, $qty);
 		}
 
 		$arr['invoice'] = load_invoice($invoice_id);
 		$arr['total'] = invoice_a_total($invoice_id);
 		$arr['products'] = load_products_invoice($invoice_id);
+
+		$total = invoice_a_total($invoice_id);
+		$from = "aelhedek1@cougars.ccis.edu";
+		//$message .= "\n Total:                                          ".$total[0]."\n";
+
+		$message = "Thank you for shopping at JeweleryBox, Order Confirmation Number: ".$invoice_id."\n";
+		$subject = "Your Order From JeweleryBox";
+		
+		$mail = "To: $email\nFrom: $from \nSubject: $subject\n\n$message";
+
+		exec("echo -e '$mail' | ssmtp $email");
+
+		unset($_SESSION['cart']);
 		echo json_encode($arr);
 		break;
 
